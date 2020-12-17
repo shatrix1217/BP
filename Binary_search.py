a3ad04bd4a85f4f43a4ad91aef0ca8c72acb2e12
shatrix1217 <53997721+shatrix1217@users.noreply.github.com>
2020-12-17 14:59:07 +0900

    binary_search

--- Binary_search.py
+++ Binary_search.py
@@ -7,14 +7,33 @@ def naive_search(list, target):
     return -1
 
 #binary search
-def binary_search(list, target):
-    mid_point = len(list) // 2
+def binary_search(list, target, low = None, high = None):
+    if low = None:
+        low = 0
+
+    if high = None:
+        high = len(list) - 1
+
+    if high < low:
+        return -1
+
+    mid_point = (low + high) // 2
     if list[mid_point] == target:
         return mid_point
 
     elif list[mid_point] > target:
-        return binary_search(list[:mid_point], target)
+        return binary_search(list, target, low, mid_point-1)
 
     else:
-        return binary_search(list[mid_point + 1:], target)
+        return binary_search(list, target, mid_point+1, high)
+
+
+if __name__ == "__main__":
+    list = [1, 10, 20, 24, 25, 74, 85, 97, 102]
+    target = 10
+
+    print(binary_search(list, target))
+
+
+
 
