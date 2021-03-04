ddb714e95b551318ab26338c1a7c39af544e9a8f
shatrix1217 <53997721+shatrix1217@users.noreply.github.com>
2021-03-05 00:21:44 +0900

    basic linear model builded

--- HousePricePredictor_XGBver.py
+++ HousePricePredictor_XGBver.py
@@ -1,19 +1,14 @@
 #To-do:import modules
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
+import numpy as np
+
+
 
 #remove data the missing target value, separate predictors and target.
-
 #Break off validation set from training data.
-
 #Select categorical columns with relatively low cardinality.
-
 #Select numeric columns
-
 #Keep selected columns only
-
 #One-hot encode the data (pandas)
 
 
